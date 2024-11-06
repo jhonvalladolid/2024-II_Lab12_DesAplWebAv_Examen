043ea58fd4c9ad096a3e0f1c77ae723159a12f62
jhonvalladolid <jhonvalladolidoficial@gmail.com>
2024-11-06 02:42:44 -0500

    Header actualizado para la navegación

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,32 +1,21 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  isMobileMenuOpen: boolean = false;
-
-  toggleMenu() {
-    this.isMobileMenuOpen = !this.isMobileMenuOpen;
-    const navbar = document.getElementById('navbar');
-    if (navbar) {
-      navbar.classList.toggle('navbar-mobile');
-    }
-  }
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const header = document.getElementById('header');
-    if (header) {
-      if (window.scrollY > 100) {
-        header.classList.add('header-scrolled');
-      } else {
-        header.classList.remove('header-scrolled');
-      }
-    }
-  }
+  navLinks = [
+    { icon: 'bx bx-home-alt-2 bx-sm', name: 'Inicio', url: '#hero' },
+    { icon: 'bx bxs-business bx-sm', name: 'Nosotros', url: '#about' },
+    { icon: 'bx bxs-briefcase-alt-2 bx-sm', name: 'Servicios', url: '#services' },
+    { icon: 'bx bxs-wrench bx-sm', name: 'Producto', url: '#portfolio' },
+    { icon: 'bx bxs-shield-alt-2 bx-sm', name: 'Marcas', url: '#marca' },
+    { icon: 'bx bxs-contact bx-sm', name: 'Contáctenos', url: '#contact' }
+  ];
 }
