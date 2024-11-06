b4149997d895fb69ea0513d98fc47c3ed9c3d8b9
jhonvalladolid <jhonvalladolidoficial@gmail.com>
2024-11-05 19:52:49 -0500

    Carousel y footer incorporado

---
+++ src/app/components/hero-carousel/hero-carousel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero-carousel.component.html',
+  styleUrl: './hero-carousel.component.css'
+})
+export class HeroCarouselComponent {
+
+}
